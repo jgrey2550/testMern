f2d9e0815cd1c16d433d74f71bc2277c0d618073
jgrey2550 <132170409+jgrey2550@users.noreply.github.com>
2023-07-20 20:52:48 -0700

    Update index.js
    https://test-mern-frontend.vercel.app/

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const RegisterModel = require('./models/Register')
 const app = express()
 app.use(cors(
     {
-        origin: ["https://test-mern-api.vercel.app"],
+        origin: ["https://test-mern-frontend.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
