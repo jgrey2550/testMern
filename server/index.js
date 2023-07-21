b23006ed467d4ec8056b716dad033c586ce774a9
jgrey2550 <132170409+jgrey2550@users.noreply.github.com>
2023-07-20 21:07:52 -0700

    Update index.js
    changed database

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://jgrey2550:2fMVM2UaMshJPmZ8@cluster0.rvseirq.mongodb.net/?retryWrites=true&w=majority');
 
 
 app.get("/", (req, res) => {
